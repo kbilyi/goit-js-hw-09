bafeac5e5c15fdf542f69caa4dd6d0ccfb152e87
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-03-18 20:35:36 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,47 +1,8 @@
-import Notiflix from 'notiflix';
-
-const refs = {
-  form: document.querySelector('form.form'),
-  delay: document.querySelector('input[name="delay"]'),
-  step: document.querySelector('input[name="step"]'),
-  amount: document.querySelector('input[name="amount"]'),
-}
-
-refs.form.addEventListener('submit', onSubmitBtn);
-
-function onSubmitBtn(event) {
-  event.preventDefault();
-
-  let firstDeleyValue = Number(refs.delay.value);  
-  let stepDeleyValue = Number(refs.step.value);
-  let amountValue = refs.amount.value;  
-
-  for (let position = 1; position <= amountValue; position += 1){
-    let delay;
-
-    if (position > 1) {
-      firstDeleyValue += stepDeleyValue;
-    }
-
-    delay = firstDeleyValue;
-
-    createPromise(position, delay)
-      .then(value => Notiflix.Notify.success(value))
-      .catch(error => Notiflix.Notify.warning(error));
-  }  
-}
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
-    }, delay);
-  });
-  return promise;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
 }
-
